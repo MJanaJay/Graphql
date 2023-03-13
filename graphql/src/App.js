00482a4e1fae9183af641b1c04207057e9f45dad
testingapplicant <mayajwork@outlook.com>
2023-03-13 05:21:42 +0000

    feat: app.js reinstated

--- graphql/src/App.js
+++ graphql/src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
+// import { Grid } from '@mui/material';
+import Skills from './components/Skillsdata';
+import Profile from './components/Profile';
+import Grades from './components/Grades';
+import Projects from './components/Projects';
+import {createTheme} from '@mui/material/styles';
 import './App.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const theme = createTheme ({
+  palette: {
+    primary: {
+      // light purple
+      // main:"#1F1045", bg color
+      main:"#956BFF",
+    },
+    secondary: {
+      // yellowish
+      main:"#F7E731",
+    },
+    tertiary: {
+      // darker purple
+      main:"#8050FA",
+    },
+  },
+  typography: {
+    fontFamily: [
+      // body font
+      'Dosis'
+    ],
+    h4: {
+      fontWeight: 600,
+      fontSize: 28,
+      lineHeight: '2rem',
+    },
+    h5: {
+      fontWeight: 100,
+      lineHeight: '2rem',
+    },
+  },
+})
+
+const client = new QueryClient();
+const endpoint = "https://learn.01founders.co/api/graphql-engine/v1/graphql";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={client}>
+      <Profile endpoint={endpoint}/>
+      <Projects endpoint={endpoint}/>
+      <Skills endpoint={endpoint}/>
+      <Grades endpoint={endpoint}/>
+    </QueryClientProvider>
+
   );
 }
 
